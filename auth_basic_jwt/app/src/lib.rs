00bc3942c70cf330618101eebb6aca04312985e0
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-08-14 01:45:55 +0100

    chore: configure postgresql

--- auth_basic_jwt/app/src/lib.rs
+++ auth_basic_jwt/app/src/lib.rs
@@ -1,16 +1,18 @@
 use anyhow::Ok;
-use axum::{routing::get, Router, };
+use axum::{routing::get, Router};
 pub struct Server;
 
-
 impl Server {
-    pub async fn run()-> anyhow::Result<()>{
-       let app = Router::new().route("/health", get(|| async { "Service healthy"}));
+    pub async fn run() -> anyhow::Result<()> {
+        let app = Router::new().route("/health", get(|| async { "Service healthy" }));
 
-       let listner =tokio::net::TcpListener::bind("0.0.0.0:3000").await .expect("error starting the application on port 3000");
+        let listner = tokio::net::TcpListener::bind("0.0.0.0:3000")
+            .await
+            .expect("error starting the application on port 3000");
 
-       axum::serve(listner, app).await.unwrap();
+        println!("server listening on http://0.0.0.0:3000\nhealth chack at http://0.0.0.0:3000/health");
+        axum::serve(listner, app).await.unwrap();
 
-        Ok(()) 
+        Ok(())
     }
 }
--- auth_basic_jwt/src/main.rs
+++ auth_basic_jwt/src/main.rs
@@ -1,5 +1,6 @@
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
     app::Server::run()
         .await
         .expect("error starting the application")
