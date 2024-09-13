b70e4a35b25fd606c3f56047a9516913ab1d7bbc
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-09-13 14:49:49 +0100

    linting fixes

--- 01_basic_server/src/lib.rs
+++ 01_basic_server/src/lib.rs
@@ -1,3 +1,3 @@
 pub mod app;
+pub mod handler;
 pub mod router;
-pub mod  handler;
--- 01_basic_server/src/router.rs
+++ 01_basic_server/src/router.rs
@@ -1,6 +1,6 @@
-use axum::{routing::get, Router};
 use super::handler::hello_world_handler;
+use axum::{routing::get, Router};
 
-pub fn router( ) -> Router {
+pub fn router() -> Router {
     Router::new().route("/", get(hello_world_handler))
 }
